4aede86143c923dbb490b9370a341d56786c4456
José Rubén <joserujume@gmail.com>
2025-08-10 23:18:40 -0600

    feat (callback): return the current first callback

--- api/validate-return.js
+++ api/validate-return.js
@@ -15,11 +15,11 @@ export default async function handler(req, res) {
     return res.status(405).json({ error: "Método no permitido" });
   }
 
-  const { returnTo, clientId } = req.body;
+  // const { returnTo, clientId } = req.body;
 
-  if (!returnTo || !clientId) {
-    return res.status(400).json({ error: "Faltan parámetros returnTo o clientId" });
-  }
+  // if (!returnTo || !clientId) {
+  //   return res.status(400).json({ error: "Faltan parámetros returnTo o clientId" });
+  // }
 
   try {
     // 1. Obtener token del Management API de Auth0
@@ -63,17 +63,26 @@ export default async function handler(req, res) {
     }
 
     const clientData = await clientRes.json();
+    console.log("clientData", clientData);
     const allowedCallbacks = clientData.callbacks || [];
 
     // 3. Validar que el returnTo esté exactamente en la lista
-    if (allowedCallbacks.includes(returnTo)) {
-      return res.status(200).json({ valid: true, safeUrl: returnTo });
+    if (allowedCallbacks.length > 0) {
+      return res.status(200).json({ valid: true, safeUrl: allowedCallbacks[0] });
     } else {
       return res.status(400).json({
         valid: false,
         error: "URL no permitida en los callbacks de la aplicación",
       });
     }
+    // if (allowedCallbacks.includes(returnTo)) {
+    //   return res.status(200).json({ valid: true, safeUrl: returnTo });
+    // } else {
+    //   return res.status(400).json({
+    //     valid: false,
+    //     error: "URL no permitida en los callbacks de la aplicación",
+    //   });
+    // }
   } catch (err) {
     console.error("Error validando returnTo:", err);
     res.status(500).json({ error: "Error interno al validar returnTo" });
